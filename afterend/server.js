16d5e83862a17ad1387fee12d5e755eefc83156a
Carb123E <2564875163@qq.com>
2024-10-07 15:00:58 +0800

    Update the api interface

--- afterend/server.js
+++ afterend/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-//2.设置跨域访问
 app.all('*', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -12,22 +11,16 @@ app.all('*', function(req, res, next) {
 	next();
 });
 
-//解决接受到前端req数据为{}的问题
 app.use(express.json())
 app.use(cors())
 
-let product = require('./page/product.js');//菜单信息
+let product = require('./page/product.js');
 app.use(product);
-let order = require('./page/order.js');//订单信息
+let order = require('./page/order.js');
 app.use(order);
-let common = require('./page/common.js');//公共模块
-app.use(common);
-let user = require('./page/user.js');//用户模块
-app.use(user);
 
 module.exports = app
 const port = 9012
-//3.1 监听端口
 app.listen(port, () => {
 	console.log(`app is running at http://localhost:${port}`)
 })
