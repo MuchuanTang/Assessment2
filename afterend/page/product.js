9d01fe1a9fa91aa1484f99d2c8fabe6d10945fb8
Carb123E <2564875163@qq.com>
2024-10-07 15:10:07 +0800

    Modify the product api prompt

--- afterend/page/product.js
+++ afterend/page/product.js
@@ -2,7 +2,7 @@
 var app = require("express").Router();
 const DBPool = require('./connDB.js')
 
-//查询产品列表
+//productListQuery
 app.get("/api/product/productListQuery", (req, res) => {
 	let param = req.query
 	let searchSql
@@ -16,7 +16,7 @@ app.get("/api/product/productListQuery", (req, res) => {
 		res.send(result);
 	})
 });
-//根据id查询产品详情
+//queryProductDetails
 app.get("/api/product/queryProductDetails", (req, res) => {
 	let searchSql = 'SELECT * FROM `productlist` WHERE name = ?'
 	let param = req.query
@@ -25,38 +25,38 @@ app.get("/api/product/queryProductDetails", (req, res) => {
 		res.send(result);
 	})
 });
-//添加产品数据
+//addProduct
 app.post('/api/product/addProduct', (req, res) => {
 	let param = req.body
 	let addSql =
 		'INSERT INTO `productlist`(`id`,`name`,`des`,`price`,`creatDate`,`supplier`,`lastNum`) VALUES(?,?,?,?,?,?,?)';
 	let addSqlParams = [new Date().getTime(), param.name, param.des, param.price, param.creatDate,param.supplier, param.lastNum];
 	DBPool.query(addSql, addSqlParams, (results) => {
-		res.send("添加数据成功！！");
+		res.send("success");
 	})
 });
-//更新产品数据
+//updataProduct
 app.post('/api/product/updataProduct', (req, res) => {
 	let param = req.body
 	DBPool.query('UPDATE productlist SET ? WHERE id = ?', [param, param.id], (results) => {
 		let result = results.results
-		res.send("修改数据成功！！")
+		res.send("success")
 	})
 });
-//更新产品库存数
+//updataProductNum
 app.post('/api/product/updataProductNum', (req, res) => {
 	let param = req.body
 	DBPool.query('UPDATE productlist SET lastNum = ? WHERE name = ?', [param.lastNum, param.name], (results) => {
 		let result = results.results
-		res.send("修改数据成功！！")
+		res.send("success")
 	})
 });
-//删除产品数据
+//deleteProduct
 app.get('/api/product/deleteProduct', (req, res) => {
 	let param = req.query
 	DBPool.query('delete from productlist where id = ?', [param.id], (results) => {
 		let result = results.results
-		res.send("数据删除成功！！")
+		res.send("success")
 	})
 });
 
